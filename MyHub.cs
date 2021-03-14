a216501b31feeb724e9a2d18bbf911e905129259
Kishore <nandkishworks@gmail.com>
2021-03-14 11:02:06 +0530

    first commit

---
+++ MyHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningSignalR1
+{
+    public class MyHub:Microsoft.AspNet.SignalR.Hub
+    {
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message);
+        }
+    }
+}
---
+++ Scripts/SignalR.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("It worked")
+            $.connection.myHub.server.announce("Connected");
+        })
+        .fail(function () { alert("Its not working") });
+
+    $.connection.myHub.client.announce = function (message) {
+        alert(message);
+    }
+})()
